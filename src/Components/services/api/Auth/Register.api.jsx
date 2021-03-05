99fe8d71fc2c2ea45e5907cccc6b979a462c414f
salar-56 <salar.nili097@gmail.com>
2021-03-05 10:59:18 +0330

    s

--- src/Components/services/api/Auth/Register.api.jsx
+++ src/Components/services/api/Auth/Register.api.jsx
@@ -5,7 +5,8 @@ const MainUrl = process.env.REACT_APP_PUBLIC_PATH;
 export const RegisterUser = async (userRegiste) => {
   try {
     // call api
-    await http.post(MainUrl + "auth/register", userRegiste);
+    await http.post(MainUrl + 
+      , userRegiste);
   } catch (error) {
 
     // return empty object if api faill
